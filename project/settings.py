705098ad90ba9cf80caa0b5b7de44c630a4446be
Olga <xotaoshi@gmail.com>
2026-07-03 16:13:24 +0300

    modified:   project/settings.py

--- project/settings.py
+++ project/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("DJANGO_SECRET")
+'i forgot '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
